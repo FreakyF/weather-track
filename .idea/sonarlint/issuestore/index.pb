5d4e9ea7706ff300ac5214b5e9995f74deb9a94d
FreakyF <100375045+FreakyF@users.noreply.github.com>
2023-07-11 23:32:54 +0200

    #19: Modify config file to make asynchronous logging, change the logging level, change folder structure of logs.

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,13 @@
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 N
 src/main/resources/logback.xml,7\0\7097ec3a5185ee6ac4acfdc815ac0ecc28af4a67
+w
+G.idea/sonarlint/issuestore/8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d,b\4\b4a0b63ce8560bbc5de0f9995c20cc861dc2adb3
+Å
+Q.idea/sonarlint/securityhotspotstore/8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d,1\0\10a03f31c4dce92cb371e761ce9429428bb6c176
+Å
+Q.idea/sonarlint/securityhotspotstore/7/0/7097ec3a5185ee6ac4acfdc815ac0ecc28af4a67,f\e\fee7fa8ec07513a1d1b91ba305a9c8cd59c5cbd0
+w
+G.idea/sonarlint/issuestore/7/0/7097ec3a5185ee6ac4acfdc815ac0ecc28af4a67,0\b\0b94eed6ac949458337e2826f9ae5d20679c8636
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
--- src/main/java/org/weathertrack/Main.java
+++ src/main/java/org/weathertrack/Main.java
@@ -7,12 +7,13 @@ public class Main {
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) {
+		String logInfo = "Example log from {}";
 		for(int i = 0; i < 10000; i++) {
-			logger.trace("Example log from {}", Main.class.getSimpleName());
-			logger.info("Example log from {}", Main.class.getSimpleName());
-			logger.debug("Example log from {}", Main.class.getSimpleName());
-			logger.warn("Example log from {}", Main.class.getSimpleName());
-			logger.error("Example log from {}", Main.class.getSimpleName());
+			logger.trace(logInfo, Main.class.getSimpleName());
+			logger.info(logInfo, Main.class.getSimpleName());
+			logger.debug(logInfo, Main.class.getSimpleName());
+			logger.warn(logInfo, Main.class.getSimpleName());
+			logger.error(logInfo, Main.class.getSimpleName());
 		}
 	}
 }
