2ca16ffbb6cc6731a93f53801999c15da2d54d77
FreakyF <100375045+FreakyF@users.noreply.github.com>
2023-07-11 21:23:08 +0200

    #19: Configure logback.xml and test logging framework.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+N
+src/main/resources/logback.xml,7\0\7097ec3a5185ee6ac4acfdc815ac0ecc28af4a67
--- src/main/java/org/weathertrack/Main.java
+++ src/main/java/org/weathertrack/Main.java
@@ -1,7 +1,18 @@
 package org.weathertrack;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
 	public static void main(String[] args) {
-		System.out.println("Hello and welcome!");
+		for(int i = 0; i < 100; i++) {
+			logger.trace("Example log from {}", Main.class.getSimpleName());
+			logger.info("Example log from {}", Main.class.getSimpleName());
+			logger.debug("Example log from {}", Main.class.getSimpleName());
+			logger.warn("Example log from {}", Main.class.getSimpleName());
+			logger.error("Example log from {}", Main.class.getSimpleName());
+		}
 	}
 }
