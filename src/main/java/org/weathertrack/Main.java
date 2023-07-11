843b910102427a8d6b9c5fb3487398aff9d1510c
FreakyF <100375045+FreakyF@users.noreply.github.com>
2023-07-11 21:32:15 +0200

    #19: Fix max file size and max total size.

--- src/main/java/org/weathertrack/Main.java
+++ src/main/java/org/weathertrack/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) {
-		for(int i = 0; i < 100; i++) {
+		for(int i = 0; i < 10000; i++) {
 			logger.trace("Example log from {}", Main.class.getSimpleName());
 			logger.info("Example log from {}", Main.class.getSimpleName());
 			logger.debug("Example log from {}", Main.class.getSimpleName());
