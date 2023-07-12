a3cdc8238bb53840903396a7d4c1a7e5a11789cb
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-12 19:17:50 +0200

    #19: Remove redundant code.

--- src/main/java/org/weathertrack/Main.java
+++ src/main/java/org/weathertrack/Main.java
@@ -7,13 +7,5 @@ public class Main {
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) {
-		String logInfo = "Example log from {}";
-		for(int i = 0; i < 10000; i++) {
-			logger.trace(logInfo, Main.class.getSimpleName());
-			logger.info(logInfo, Main.class.getSimpleName());
-			logger.debug(logInfo, Main.class.getSimpleName());
-			logger.warn(logInfo, Main.class.getSimpleName());
-			logger.error(logInfo, Main.class.getSimpleName());
-		}
 	}
 }
