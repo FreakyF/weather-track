2ca022e94ab600e968cad571bb1abdd3266fcd78
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-14 20:35:48 +0200

    #3: Add userInputService via dependency injection and modify printWeather formatting.

--- src/main/java/org/weathertrack/view/cli/CommandLineUserInterface.java
+++ src/main/java/org/weathertrack/view/cli/CommandLineUserInterface.java
@@ -1,26 +1,25 @@
 package org.weathertrack.view.cli;
 
 import org.weathertrack.model.WeatherData;
+import org.weathertrack.service.input.UserInputService;
 import org.weathertrack.view.UserInterface;
 import org.weathertrack.view.util.WeatherUIResources;
 
 import java.util.List;
-import java.util.Scanner;
 
 @SuppressWarnings("java:S106")
 public class CommandLineUserInterface implements UserInterface {
-	private final Scanner scanner;
+	private final UserInputService userInputService;
 
-	public CommandLineUserInterface() {
-		this.scanner = new Scanner(System.in);
+	public CommandLineUserInterface(UserInputService userInputService) {
+		this.userInputService = userInputService;
 	}
 
 	@Override
 	public String getCityNameFromUser() {
 		String cityName;
 		do {
-			System.out.print("Enter the city name: ");
-			cityName = scanner.nextLine().trim();
+			cityName = userInputService.getUserInput("Enter the city name: ").trim();
 		} while (cityName.isBlank());
 
 		return cityName;
@@ -40,25 +39,47 @@ public class CommandLineUserInterface implements UserInterface {
 
 	@Override
 	public void printWeather(WeatherData weatherData) {
-		var weatherConditionMessage = String.format(WeatherUIResources.WEATHER_CONDITION, weatherData.getWeatherCondition());
+		var weatherConditionMessage = String.format(
+				WeatherUIResources.WEATHER_CONDITION,
+				weatherData.getWeatherCondition()
+		);
 		System.out.println(weatherConditionMessage);
 
-		var temperatureMessage = String.format(WeatherUIResources.TEMPERATURE, weatherData.getTemperature());
+		var temperatureMessage = String.format(
+				WeatherUIResources.TEMPERATURE,
+				weatherData.getTemperature()
+		);
 		System.out.println(temperatureMessage);
 
-		var cloudinessMessage = String.format(WeatherUIResources.CLOUDINESS, weatherData.getCloudiness());
+		var cloudinessMessage = String.format(
+				WeatherUIResources.CLOUDINESS,
+				weatherData.getCloudiness()
+		);
 		System.out.println(cloudinessMessage);
 
-		var rainChanceMessage = String.format(WeatherUIResources.RAIN_CHANCE, weatherData.getRainChance());
+		var rainChanceMessage = String.format(
+				WeatherUIResources.RAIN_CHANCE,
+				weatherData.getRainChance()
+		);
+
 		System.out.println(rainChanceMessage);
 
-		var windSpeedMessage = String.format(WeatherUIResources.WIND_SPEED, weatherData.getWindSpeed());
+		var windSpeedMessage = String.format(
+				WeatherUIResources.WIND_SPEED,
+				weatherData.getWindSpeed()
+		);
 		System.out.println(windSpeedMessage);
 
-		var humidityMessage = String.format(WeatherUIResources.HUMIDITY, weatherData.getHumidity());
+		var humidityMessage = String.format(
+				WeatherUIResources.HUMIDITY,
+				weatherData.getHumidity()
+		);
 		System.out.println(humidityMessage);
 
-		var pressureMessage = String.format(WeatherUIResources.PRESSURE, weatherData.getPressure());
+		var pressureMessage = String.format(
+				WeatherUIResources.PRESSURE,
+				weatherData.getPressure()
+		);
 		System.out.println(pressureMessage);
 	}
 }
