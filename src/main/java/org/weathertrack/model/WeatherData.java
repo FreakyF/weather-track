d4f22c06476711e577e7bb463547eae55a109241
FreakyF <100375045+FreakyF@users.noreply.github.com>
2023-07-14 16:14:14 +0200

    #4: Modify data types for WeatherData fields.

--- src/main/java/org/weathertrack/model/WeatherData.java
+++ src/main/java/org/weathertrack/model/WeatherData.java
@@ -1,55 +1,55 @@
 package org.weathertrack.model;
 
 public class WeatherData {
-	private final String temperature;
-	private final String cloudiness;
-	private final String rainChance;
-	private final String windSpeed;
 	private final String weatherCondition;
-	private final String humidity;
-	private final String pressure;
+	private final float temperature;
+	private final int cloudiness;
+	private final int rainChance;
+	private final float windSpeed;
+	private final int humidity;
+	private final int pressure;
 
-	public WeatherData(String temperature,
-	                   String cloudiness,
-	                   String rainChance,
-	                   String windSpeed,
-	                   String weatherCondition,
-	                   String humidity,
-	                   String pressure) {
+	public WeatherData(String weatherCondition,
+	                   float temperature,
+	                   int cloudiness,
+	                   int rainChance,
+	                   float windSpeed,
+	                   int humidity,
+	                   int pressure) {
+		this.weatherCondition = weatherCondition;
 		this.temperature = temperature;
 		this.cloudiness = cloudiness;
 		this.rainChance = rainChance;
 		this.windSpeed = windSpeed;
-		this.weatherCondition = weatherCondition;
 		this.humidity = humidity;
 		this.pressure = pressure;
-	}
-
-	public String getTemperature() {
-		return temperature;
-	}
-
-	public String getCloudiness() {
-		return cloudiness;
-	}
-
-	public String getRainChance() {
-		return rainChance;
-	}
-
-	public String getWindSpeed() {
-		return windSpeed;
 	}
 
 	public String getWeatherCondition() {
 		return weatherCondition;
 	}
 
-	public String getHumidity() {
+	public float getTemperature() {
+		return temperature;
+	}
+
+	public int getCloudiness() {
+		return cloudiness;
+	}
+
+	public int getRainChance() {
+		return rainChance;
+	}
+
+	public float getWindSpeed() {
+		return windSpeed;
+	}
+
+	public int getHumidity() {
 		return humidity;
 	}
 
-	public String getPressure() {
+	public int getPressure() {
 		return pressure;
 	}
 }
