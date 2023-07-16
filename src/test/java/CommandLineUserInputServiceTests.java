28d531f1542dcbe29da4280f3087de41b460703d
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-16 19:04:15 +0200

    #3: Rename UserInputTest.java to CommandLineUserInputServiceTests.

--- src/test/java/CommandLineUserInputServiceTests.java
+++ src/test/java/CommandLineUserInputServiceTests.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class UserInputTest {
+class CommandLineUserInputServiceTests {
 	private CommandLineUserInputService userInputService;
 	private Scanner mockScanner;
 
