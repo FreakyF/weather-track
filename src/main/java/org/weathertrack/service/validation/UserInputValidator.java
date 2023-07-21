16b248925673470edeb0490d084b02d8ab552dfa
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-21 17:38:45 +0200

    #22: Create UserInputvalidator class.

---
+++ src/main/java/org/weathertrack/service/validation/UserInputValidator.java
@@ -0,0 +1,15 @@
+package org.weathertrack.service.validation;
+
+import org.weathertrack.service.validation.model.InputValidationResult;
+
+public class UserInputValidator implements InputValidator {
+	@Override
+	public InputValidationResult validateCityNameInput() {
+		return null;
+	}
+
+	@Override
+	public InputValidationResult validateMenuEntryInput() {
+		return null;
+	}
+}
