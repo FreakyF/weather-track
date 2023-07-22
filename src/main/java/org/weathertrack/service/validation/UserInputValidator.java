2ee1d13c6d642bef1d35463d148219f15c82ebc3
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-22 13:39:11 +0200

    #22: Implement validateCityNameInput method and validateMenuEntryInput.

--- src/main/java/org/weathertrack/service/validation/UserInputValidator.java
+++ src/main/java/org/weathertrack/service/validation/UserInputValidator.java
@@ -1,15 +1,48 @@
 package org.weathertrack.service.validation;
 
 import org.weathertrack.service.validation.model.InputValidationResult;
+import org.weathertrack.service.validation.util.ValidationMessage;
+
+import java.util.regex.Pattern;
 
 public class UserInputValidator implements InputValidator {
+	private static final int MAXIMUM_CITY_NAME_LENGTH = 170;
+	InputValidationResult inputValidationResult;
+
 	@Override
 	public InputValidationResult validateCityNameInput(String userCityInput) {
-		return null;
+		// check for null
+		if (userCityInput == null) {
+			throw new IllegalStateException(InputValidationExceptionMessage.USER_INPUT_IS_NULL);
+		}
+		// check if it's empty or not trimmed
+		if (userCityInput.isBlank()) {
+			inputValidationResult = new InputValidationResult(false, ValidationMessage.CITY_INPUT_EMPTY);
+			return inputValidationResult;
+		}
+		// check maximum length
+		if (userCityInput.length() > MAXIMUM_CITY_NAME_LENGTH) {
+			inputValidationResult = new InputValidationResult(false, ValidationMessage.CITY_INPUT_TOO_LONG);
+			return inputValidationResult;
+		}
+		// check for valid characters
+		Pattern pattern = Pattern.compile("[\\p{L}'\\- ]+");
+		if (!pattern.matcher(userCityInput).matches()) {
+			inputValidationResult = new InputValidationResult(false, ValidationMessage.CITY_INPUT_CONTAINS_SPECIAL_CHARACTERS);
+			return inputValidationResult;
+		}
+
+		inputValidationResult = new InputValidationResult(true, null);
+		return inputValidationResult;
 	}
 
 	@Override
 	public InputValidationResult validateMenuEntryInput(int userMenuEntry) {
-		return null;
+		if (userMenuEntry > 0) {
+			inputValidationResult = new InputValidationResult(true, ValidationMessage.MENU_ENTRY_IS_ZERO);
+			return inputValidationResult;
+		}
+		inputValidationResult = new InputValidationResult(true, null);
+		return inputValidationResult;
 	}
 }
