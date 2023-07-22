3b4c1d78ec855d983dd7fa5699c24a824a21b0b7
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-22 15:12:32 +0200

    #22: Change the Format of the ValidationMessage.

--- src/main/java/org/weathertrack/service/validation/UserInputValidator.java
+++ src/main/java/org/weathertrack/service/validation/UserInputValidator.java
@@ -19,23 +19,23 @@ public class UserInputValidator implements InputValidator {
 		if (userCityInput.isBlank()) {
 			return new InputValidationResult(
 					false,
-					ValidationMessage.CITY_INPUT_EMPTY + ValidationMessage.PLEASE_TRY_AGAIN);
+					ValidationMessage.CITY_INPUT_EMPTY + " " + ValidationMessage.PLEASE_TRY_AGAIN);
 
 		}
 		if (userCityInput.length() > MAXIMUM_CITY_NAME_LENGTH) {
 			return new InputValidationResult(
 					false,
-					ValidationMessage.CITY_INPUT_TOO_LONG + ValidationMessage.PLEASE_TRY_AGAIN);
+					ValidationMessage.CITY_INPUT_TOO_LONG + " " + ValidationMessage.PLEASE_TRY_AGAIN);
 		}
 		if (CITY_NAME_PATTERN_NUMBERS.matcher(userCityInput).matches()) {
 			return new InputValidationResult(
 					false,
-					ValidationMessage.CITY_INPUT_CONTAINS_NUMBERS + ValidationMessage.PLEASE_TRY_AGAIN);
+					ValidationMessage.CITY_INPUT_CONTAINS_NUMBERS + " " + ValidationMessage.PLEASE_TRY_AGAIN);
 		}
 		if (CITY_NAME_PATTERN_SPECIAL_CHARACTERS.matcher(userCityInput).matches()) {
 			return new InputValidationResult(
 					false,
-					ValidationMessage.CITY_INPUT_CONTAINS_SPECIAL_CHARACTERS + ValidationMessage.PLEASE_TRY_AGAIN);
+					ValidationMessage.CITY_INPUT_CONTAINS_SPECIAL_CHARACTERS + " " + ValidationMessage.PLEASE_TRY_AGAIN);
 		}
 
 		return new InputValidationResult(true, null);
@@ -46,7 +46,7 @@ public class UserInputValidator implements InputValidator {
 		if (userMenuEntry <= 0) {
 			return new InputValidationResult(
 					false,
-					ValidationMessage.MENU_ENTRY_IS_ZERO + ValidationMessage.PLEASE_TRY_AGAIN);
+					ValidationMessage.MENU_ENTRY_IS_ZERO + " " + ValidationMessage.PLEASE_TRY_AGAIN);
 		}
 		return new InputValidationResult(true, null);
 	}
