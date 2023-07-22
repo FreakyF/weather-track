48f6812cd8dc02a0f73c65d1933a7845c0a6f622
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-22 19:50:47 +0200

    #22: Remove unused USER_MENU_ENTRY_IS_ZERO message.

--- src/main/java/org/weathertrack/service/validation/InputValidationExceptionMessage.java
+++ src/main/java/org/weathertrack/service/validation/InputValidationExceptionMessage.java
@@ -4,9 +4,6 @@ public class InputValidationExceptionMessage {
 	public static final String USER_INPUT_IS_NULL =
 			"User input is null!";
 
-	public static final String USER_MENU_ENTRY_IS_ZERO =
-			"User menu entry is zero!";
-
 	private InputValidationExceptionMessage() {
 	}
 }
