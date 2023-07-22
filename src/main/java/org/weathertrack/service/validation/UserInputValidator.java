4e591838889d476e052a9dc853029352edc32235
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-22 15:14:25 +0200

    #22: Remove PLEASE_TRY_AGAIN message.

--- src/main/java/org/weathertrack/service/validation/UserInputValidator.java
+++ src/main/java/org/weathertrack/service/validation/UserInputValidator.java
@@ -17,25 +17,18 @@ public class UserInputValidator implements InputValidator {
 			throw new IllegalArgumentException(InputValidationExceptionMessage.USER_INPUT_IS_NULL);
 		}
 		if (userCityInput.isBlank()) {
-			return new InputValidationResult(
-					false,
-					ValidationMessage.CITY_INPUT_EMPTY + " " + ValidationMessage.PLEASE_TRY_AGAIN);
+			return new InputValidationResult(false, ValidationMessage.CITY_INPUT_EMPTY);
 
 		}
 		if (userCityInput.length() > MAXIMUM_CITY_NAME_LENGTH) {
-			return new InputValidationResult(
-					false,
-					ValidationMessage.CITY_INPUT_TOO_LONG + " " + ValidationMessage.PLEASE_TRY_AGAIN);
+			return new InputValidationResult(false, ValidationMessage.CITY_INPUT_TOO_LONG);
 		}
 		if (CITY_NAME_PATTERN_NUMBERS.matcher(userCityInput).matches()) {
 			return new InputValidationResult(
-					false,
-					ValidationMessage.CITY_INPUT_CONTAINS_NUMBERS + " " + ValidationMessage.PLEASE_TRY_AGAIN);
+					false, ValidationMessage.CITY_INPUT_CONTAINS_NUMBERS);
 		}
 		if (CITY_NAME_PATTERN_SPECIAL_CHARACTERS.matcher(userCityInput).matches()) {
-			return new InputValidationResult(
-					false,
-					ValidationMessage.CITY_INPUT_CONTAINS_SPECIAL_CHARACTERS + " " + ValidationMessage.PLEASE_TRY_AGAIN);
+			return new InputValidationResult(false, ValidationMessage.CITY_INPUT_CONTAINS_SPECIAL_CHARACTERS);
 		}
 
 		return new InputValidationResult(true, null);
@@ -44,9 +37,7 @@ public class UserInputValidator implements InputValidator {
 	@Override
 	public InputValidationResult validateMenuEntryInput(int userMenuEntry) {
 		if (userMenuEntry <= 0) {
-			return new InputValidationResult(
-					false,
-					ValidationMessage.MENU_ENTRY_IS_ZERO + " " + ValidationMessage.PLEASE_TRY_AGAIN);
+			return new InputValidationResult(false, ValidationMessage.MENU_ENTRY_IS_ZERO);
 		}
 		return new InputValidationResult(true, null);
 	}
