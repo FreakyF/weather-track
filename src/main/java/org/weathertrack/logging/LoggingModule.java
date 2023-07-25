77e4d05660061eacf77b151d51add207507e71dd
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-25 15:05:36 +0200

    #30: Rename LoggerModule.java to LoggingModule.java

--- src/main/java/org/weathertrack/logging/LoggingModule.java
+++ src/main/java/org/weathertrack/logging/LoggingModule.java
@@ -4,9 +4,10 @@ import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
 import org.weathertrack.input.service.userio.CommandLineUserIOService;
 
-public class LoggerModule extends AbstractModule {
+public class LoggingModule extends AbstractModule {
 	@Override
 	protected void configure() {
-		bind(new TypeLiteral<Logger<CommandLineUserIOService>>() {}).toProvider(() -> new LogbackLogger<>(CommandLineUserIOService.class));
+		bind(new TypeLiteral<Logger<CommandLineUserIOService>>() {
+		}).toProvider(() -> new LogbackLogger<>(CommandLineUserIOService.class));
 	}
 }
