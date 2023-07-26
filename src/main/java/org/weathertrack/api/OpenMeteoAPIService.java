8597a59e89106db85e994c8e55d269e60960c3b5
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-26 16:16:09 +0200

    #6: Refactor fetchCoordinatesFromCityName method.

--- src/main/java/org/weathertrack/api/OpenMeteoAPIService.java
+++ src/main/java/org/weathertrack/api/OpenMeteoAPIService.java
@@ -8,17 +8,20 @@ import org.weathertrack.api.model.Coordinates;
 import org.weathertrack.weather.model.WeatherData;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Optional;
 
 public class OpenMeteoAPIService implements APIService {
 	private static final String GEOCODING_API_ENDPOINT = "https://geocoding-api.open-meteo.com/v1/search";
-	private static final String WEATHER_API_ENDPOINT = "https://geocoding-api.open-meteo.com/v1/forecast";
+	private static final String RESULTS_KEY = "results";
 
 	@Override
 	public WeatherData fetchWeatherFromCoordinates(Coordinates coordinates) {
-		// Implement weather data retrieval from the weather API here.
 		return null;
 	}
 
@@ -26,30 +29,45 @@ public class OpenMeteoAPIService implements APIService {
 	public Coordinates fetchCoordinatesFromCityName(String cityName) {
 		String requestUrl = GEOCODING_API_ENDPOINT + "?name=" + cityName;
 		try {
-			URL url = new URL(requestUrl);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-			if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
-				Gson gson = new Gson();
-				InputStreamReader inputStreamReader = new InputStreamReader(conn.getInputStream());
-				JsonObject responseJson = gson.fromJson(inputStreamReader, JsonObject.class);
+			HttpClient httpClient = HttpClient.newHttpClient();
+			HttpRequest httpRequest = HttpRequest.newBuilder()
+					.uri(URI.create(requestUrl))
+					.GET()
+					.build();
 
-				if (responseJson.has("results") && responseJson.get("results").isJsonArray()) {
-					JsonArray resultsArray = responseJson.getAsJsonArray("results");
-					if (resultsArray.size() > 0) {
-						JsonElement firstResult = resultsArray.get(0);
-						if (firstResult.isJsonObject()) {
-							JsonObject firstResultObj = firstResult.getAsJsonObject();
-							double latitude = firstResultObj.get("latitude").getAsDouble();
-							double longitude = firstResultObj.get("longitude").getAsDouble();
-							return new Coordinates(latitude, longitude);
-						}
+			HttpResponse<InputStream> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofInputStream());
+			if (response.statusCode() == 200) {
+				Optional<Coordinates> coordinates = parseCoordinatesFromResponse(response.body());
+				return coordinates.orElse(null);
+			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private Optional<Coordinates> parseCoordinatesFromResponse(InputStream responseBody) {
+		try (InputStreamReader inputStreamReader = new InputStreamReader(responseBody)) {
+			Gson gson = new Gson();
+			JsonObject responseJson = gson.fromJson(inputStreamReader, JsonObject.class);
+
+			if (responseJson.has(RESULTS_KEY) && responseJson.get(RESULTS_KEY).isJsonArray()) {
+				JsonArray resultsArray = responseJson.getAsJsonArray(RESULTS_KEY);
+				if (resultsArray.size() > 0) {
+					JsonElement firstResult = resultsArray.get(0);
+					if (firstResult.isJsonObject()) {
+						JsonObject firstResultObj = firstResult.getAsJsonObject();
+						double latitude = firstResultObj.get("latitude").getAsDouble();
+						double longitude = firstResultObj.get("longitude").getAsDouble();
+						return Optional.of(new Coordinates(latitude, longitude));
 					}
 				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return Optional.empty();
 	}
 }
