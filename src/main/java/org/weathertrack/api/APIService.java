ede027a54dd683be7f9babdbac5f2e1b8c8106ef
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-26 16:16:34 +0200

    #6: Remove necessary comments.

--- src/main/java/org/weathertrack/api/APIService.java
+++ src/main/java/org/weathertrack/api/APIService.java
@@ -4,9 +4,7 @@ import org.weathertrack.api.model.Coordinates;
 import org.weathertrack.weather.model.WeatherData;
 
 public interface APIService {
-	// model
 	WeatherData fetchWeatherFromCoordinates(Coordinates coordinates);
 
-	// weatherForecast
 	Coordinates fetchCoordinatesFromCityName(String cityName);
 }
