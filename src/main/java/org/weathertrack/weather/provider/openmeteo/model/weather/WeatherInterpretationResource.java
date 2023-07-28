aaa1a70fc657e31088cf931e4e5bd8cf7fb24c00
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-28 11:42:20 +0200

    #6: Modify the WeatherInterpretationResource to use a Logger.

--- src/main/java/org/weathertrack/weather/provider/openmeteo/model/weather/WeatherInterpretationResource.java
+++ src/main/java/org/weathertrack/weather/provider/openmeteo/model/weather/WeatherInterpretationResource.java
@@ -1,5 +1,7 @@
 package org.weathertrack.weather.provider.openmeteo.model.weather;
 
+import org.weathertrack.logging.Logger;
+import org.weathertrack.logging.factory.LoggerFactory;
 import org.weathertrack.weather.resource.WeatherDisplayResource;
 
 import java.io.IOException;
@@ -7,20 +9,21 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class WeatherInterpretationResource {
-
+	private static Logger logger;
 	private static final String PROPERTIES_FILE = "weather_descriptions.properties";
 	private static Properties properties;
+
+	private WeatherInterpretationResource(LoggerFactory loggerFactory) {
+		logger = loggerFactory.create(WeatherInterpretationResource.class);
+	}
 
 	static {
 		try (InputStream inputStream = WeatherDisplayResource.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE)) {
 			properties = new Properties();
 			properties.load(inputStream);
 		} catch (IOException e) {
-			e.printStackTrace();
+			logger.error("Failed to load the {}", PROPERTIES_FILE, e);
 		}
-	}
-
-	private WeatherInterpretationResource() {
 	}
 
 	public static String interpretWeatherCode(int code) {
