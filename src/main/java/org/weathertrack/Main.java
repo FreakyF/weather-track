a8a437edd910eb4a5e210ae56b34b26744250953
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-28 19:24:40 +0200

    #6: Use dependency injection for OpenMeteoGeocodingProvider.

--- src/main/java/org/weathertrack/Main.java
+++ src/main/java/org/weathertrack/Main.java
@@ -2,7 +2,7 @@ package org.weathertrack;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import org.weathertrack.api.geocoding.GeocodingProvider;
+import org.weathertrack.api.geocoding.GeocodingModule;
 import org.weathertrack.api.geocoding.openmeteo.OpenMeteoGeocodingProvider;
 import org.weathertrack.api.geocoding.openmeteo.model.city.CityData;
 import org.weathertrack.input.service.userio.CommandLineUserIOService;
@@ -12,13 +12,14 @@ import org.weathertrack.logging.LoggingModule;
 import java.util.List;
 
 public class Main {
-	private static final Injector injector = Guice.createInjector(new LoggingModule(), new UserIOModule());
+	private static final Injector injectorUserIOService = Guice.createInjector(new LoggingModule(), new UserIOModule());
+	private static final Injector injectorGeocodingProvider = Guice.createInjector(new LoggingModule(), new GeocodingModule());
 
 	public static void main(String[] args) {
-		var userIOService = injector.getInstance(CommandLineUserIOService.class);
+		var userIOService = injectorUserIOService.getInstance(CommandLineUserIOService.class);
 		var cityName = userIOService.getCityNameFromUser();
 
-		GeocodingProvider geocodingProvider = new OpenMeteoGeocodingProvider();
+		var geocodingProvider = injectorGeocodingProvider.getInstance(OpenMeteoGeocodingProvider.class);
 		List<CityData> cityDataList = geocodingProvider.fetchCityData(cityName);
 
 		userIOService.printCitiesWithSameName(cityDataList);
--- src/main/java/org/weathertrack/api/geocoding/GeocodingModule.java
+++ src/main/java/org/weathertrack/api/geocoding/GeocodingModule.java
@@ -1,6 +1,11 @@
 package org.weathertrack.api.geocoding;
 
 import com.google.inject.AbstractModule;
+import org.weathertrack.api.geocoding.openmeteo.OpenMeteoGeocodingProvider;
 
 public class GeocodingModule extends AbstractModule {
+	@Override
+	protected void configure() {
+		bind(GeocodingProvider.class).to(OpenMeteoGeocodingProvider.class);
+	}
 }
