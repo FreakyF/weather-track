6931fd93ab765d28faf542ab852af6ada58e09aa
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-31 20:06:15 +0200

    #6: Add new methods for OpenMeteoGeocodingApiService.

--- src/main/java/org/weathertrack/api/service/geocoding/openmeteo/OpenMeteoGeocodingApiService.java
+++ src/main/java/org/weathertrack/api/service/geocoding/openmeteo/OpenMeteoGeocodingApiService.java
@@ -4,16 +4,34 @@ import org.weathertrack.api.service.geocoding.GeocodingApiService;
 import org.weathertrack.api.service.geocoding.model.GeocodingCityData;
 import org.weathertrack.api.service.geocoding.model.GeocodingData;
 
+import java.net.URI;
 import java.util.List;
 
 public class OpenMeteoGeocodingApiService implements GeocodingApiService {
-    @Override
-    public List<GeocodingCityData> fetchCitiesForCityName(String cityName) {
-        throw new UnsupportedOperationException("Not Implemented");
-    }
+	private static final String GEOCODING_API_SCHEME = "https";
+	private static final String GEOCODING_API_HOST = "geocoding-api.open-meteo.com";
+	private static final String GEOCODING_API_PATH = "/v1/search";
 
-    @Override
-    public GeocodingData fetchGeocodingDataForCity(GeocodingCityData geocodingCityData) {
-        throw new UnsupportedOperationException("Not Implemented");
-    }
+	@Override
+	public List<GeocodingCityData> fetchCitiesForCityName(String cityName) {
+		throw new UnsupportedOperationException("Not Implemented");
+
+	}
+
+	public List<GeocodingCityData> fetchGeocodingCityDataFromApi(URI requestUrl) {
+		throw new UnsupportedOperationException("Not Implemented");
+	}
+
+	public URI buildGeocodingApiUri(String cityName) {
+		throw new UnsupportedOperationException("Not Implemented");
+	}
+
+	public void validateCityName(String cityName) {
+		throw new UnsupportedOperationException("Not Implemented");
+	}
+
+	@Override
+	public GeocodingData fetchGeocodingDataForCity(GeocodingCityData geocodingCityData) {
+		throw new UnsupportedOperationException("Not Implemented");
+	}
 }
