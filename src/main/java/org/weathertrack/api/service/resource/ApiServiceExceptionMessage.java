e22db4d68b4d31c7d3b2dacab38cbe3824691e04
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-31 17:51:43 +0200

    #6: Add GEOCODING_CITY_DATA_IS_NULL message to ApiServiceExceptionMessage.

--- src/main/java/org/weathertrack/api/service/resource/ApiServiceExceptionMessage.java
+++ src/main/java/org/weathertrack/api/service/resource/ApiServiceExceptionMessage.java
@@ -5,6 +5,8 @@ public class ApiServiceExceptionMessage {
 			= "Tried to get city name but the city name is null!";
 	public static final String CITY_NAME_IS_BLANK
 			= "Tried to get city name but the city name is blank!";
+	public static final String GEOCODING_CITY_DATA_IS_NULL
+			= "Tried to get geocoding city data but the geocoding city data is null!";
 
 	private ApiServiceExceptionMessage() {
 	}
