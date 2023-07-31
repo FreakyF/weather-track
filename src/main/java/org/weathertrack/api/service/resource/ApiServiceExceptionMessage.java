b163d740fa537acd270a5276975ddd21f26a15ce
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-31 16:02:08 +0200

    #6: Add final keyword to CITY_NAME_IS_BLANK

--- src/main/java/org/weathertrack/api/service/resource/ApiServiceExceptionMessage.java
+++ src/main/java/org/weathertrack/api/service/resource/ApiServiceExceptionMessage.java
@@ -3,7 +3,7 @@ package org.weathertrack.api.service.resource;
 public class ApiServiceExceptionMessage {
 	public static final String CITY_NAME_IS_NULL
 			= "Tried to get city name but the city name is null!";
-	public static String CITY_NAME_IS_BLANK
+	public static final String CITY_NAME_IS_BLANK
 			= "Tried to get city name but the city name is blank!";
 
 	private ApiServiceExceptionMessage() {
