43081c0877c61102595741b3e70f1235a2c864c9
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-31 16:34:41 +0200

    #6: Partially implement fetchCitiesForCityName_WhenCityNameIsValid_ShouldReturnGeocodingCityData method.

--- src/test/java/org/weathertrack/api/service/geocoding/openmeteo/OpenMeteoGeocodingApiServiceTests.java
+++ src/test/java/org/weathertrack/api/service/geocoding/openmeteo/OpenMeteoGeocodingApiServiceTests.java
@@ -5,14 +5,19 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.weathertrack.api.service.geocoding.GeocodingApiService;
+import org.weathertrack.api.service.geocoding.model.GeocodingCityData;
 import org.weathertrack.api.service.resource.ApiServiceExceptionMessage;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
-public class OpenMeteoGeocodingApiServiceTests {
+class OpenMeteoGeocodingApiServiceTests {
 	private GeocodingApiService sut;
 
 	@BeforeEach
@@ -35,5 +40,36 @@ public class OpenMeteoGeocodingApiServiceTests {
 		// Then
 		var exception = assertThrows(exceptionClass, () -> sut.fetchCitiesForCityName(cityNameValue));
 		assertEquals(expectedExceptionMessage, exception.getMessage());
+	}
+
+	private static Stream<Arguments> fetchCitiesForCityName_WhenCityNameIsValid_ShouldReturnGeocodingCityData() {
+		return Stream.of(
+				Arguments.of("Kielce"),
+				Arguments.of("Jastrzębie-Zdrój"),
+				Arguments.of("San Francisco"),
+				Arguments.of("D'Lo"),
+				Arguments.of("Saint-Lô"),
+				Arguments.of("Winston-Salem")
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource
+	void fetchCitiesForCityName_WhenCityNameIsValid_ShouldReturnGeocodingCityData(String cityNameValue) {
+		// When
+		List<GeocodingCityData> mockResponse = new ArrayList<>();
+		mockResponse.add(new GeocodingCityData("Kielce", "Świętokrzyskie", "Poland"));
+		mockResponse.add(new GeocodingCityData("Jastrzębie-Zdrój", "Silesia", "Poland"));
+		mockResponse.add(new GeocodingCityData("San Francisco", "California", "United States"));
+		mockResponse.add(new GeocodingCityData("D'Lo", "Mississippi", "United States"));
+		mockResponse.add(new GeocodingCityData("Winston-Salem", "North Carolina", "United States"));
+
+		when(sut.fetchCitiesForCityName(anyString())).thenReturn(mockResponse);
+
+		// Given
+		var result = sut.fetchCitiesForCityName(cityNameValue);
+
+		// Then
+		assertEquals(mockResponse, result);
 	}
 }
