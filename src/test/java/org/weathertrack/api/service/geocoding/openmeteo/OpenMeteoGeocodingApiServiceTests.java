2efc9fe9f4363e3d23a390607d44620a17229b81
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-31 16:04:06 +0200

    #6: Modify fetchCitiesForCityName_WhenCityNameIsInvalid_ShouldThrowException_WithAppropriateMessage.

--- src/test/java/org/weathertrack/api/service/geocoding/openmeteo/OpenMeteoGeocodingApiServiceTests.java
+++ src/test/java/org/weathertrack/api/service/geocoding/openmeteo/OpenMeteoGeocodingApiServiceTests.java
@@ -1,27 +1,39 @@
 package org.weathertrack.api.service.geocoding.openmeteo;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.weathertrack.api.service.geocoding.GeocodingApiService;
+import org.weathertrack.api.service.resource.ApiServiceExceptionMessage;
 
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class OpenMeteoGeocodingApiServiceTests {
-    private GeocodingApiService sut;
+	private GeocodingApiService sut;
 
-    @BeforeEach
-    void beforeEach(){
-        sut = new OpenMeteoGeocodingApiService();
-    }
+	@BeforeEach
+	void beforeEach() {
+		sut = new OpenMeteoGeocodingApiService();
+	}
 
-    @Test
-    void fetchCitiesForCityName_whenCityNameIsNull_shouldThrowException() {
-        // when
-        String cityName = null;
-        var expectedExceptionMessage = "City name was null";
+	private static Stream<Arguments> fetchCitiesForCityName_WhenCityNameIsInvalid_ShouldThrowException_WithAppropriateMessage() {
+		return Stream.of(
+				Arguments.of(null, ApiServiceExceptionMessage.CITY_NAME_IS_NULL, NullPointerException.class),
+				Arguments.of("", ApiServiceExceptionMessage.CITY_NAME_IS_BLANK, IllegalArgumentException.class),
+				Arguments.of(" ", ApiServiceExceptionMessage.CITY_NAME_IS_BLANK, IllegalArgumentException.class)
+		);
+	}
 
-        // then
-        var exception = assertThrows(NullPointerException.class, () -> sut.fetchCitiesForCityName(cityName));
-        assert(exception.getMessage()).equals(expectedExceptionMessage);
-    }
+	@ParameterizedTest
+	@MethodSource
+	void fetchCitiesForCityName_WhenCityNameIsInvalid_ShouldThrowException_WithAppropriateMessage(
+			String cityNameValue, String expectedExceptionMessage, Class<? extends Throwable> exceptionClass) {
+		// Then
+		var exception = assertThrows(exceptionClass, () -> sut.fetchCitiesForCityName(cityNameValue));
+		assertEquals(expectedExceptionMessage, exception.getMessage());
+	}
 }
