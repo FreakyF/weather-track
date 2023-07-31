8742e29aee281cfe6265d2ae22286e5339dc5a0e
Kamil Fudala <kamil.fudala44@gmail.com>
2023-07-31 15:49:49 +0200

    #6: Create sendHttpGetRequest and parseJsonResponse methods in JsonHttpService class.

--- src/main/java/org/weathertrack/api/service/http/json/JsonHttpService.java
+++ src/main/java/org/weathertrack/api/service/http/json/JsonHttpService.java
@@ -2,5 +2,20 @@ package org.weathertrack.api.service.http.json;
 
 import org.weathertrack.api.service.http.HttpService;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.http.HttpResponse;
+
 public class JsonHttpService implements HttpService {
+	@Override
+	public HttpResponse<InputStream> sendHttpGetRequest(URI requestUrl) throws IOException, InterruptedException {
+		throw new UnsupportedOperationException("Not Implemented");
+	}
+
+	@Override
+	public <T> T parseJsonResponse(InputStream responseBody, Type targetType) {
+		throw new UnsupportedOperationException("Not Implemented");
+	}
 }
