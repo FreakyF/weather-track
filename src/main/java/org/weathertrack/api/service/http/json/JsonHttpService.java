3bc4dca933e98890d53c2e29e09ca5ed7b16a4ea
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-01 17:17:00 +0200

    #6: Add @Inject to constructor and implement methods.

--- src/main/java/org/weathertrack/api/service/http/json/JsonHttpService.java
+++ src/main/java/org/weathertrack/api/service/http/json/JsonHttpService.java
@@ -1,28 +1,44 @@
 package org.weathertrack.api.service.http.json;
 
+import com.google.gson.Gson;
+import com.google.inject.Inject;
 import org.weathertrack.api.service.http.HttpService;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class JsonHttpService implements HttpService {
 	private final HttpClient httpClient;
 
+	@Inject
 	public JsonHttpService(HttpClient httpClient) {
 		this.httpClient = httpClient;
 	}
 
 	@Override
 	public HttpResponse<InputStream> sendHttpGetRequest(URI requestUrl) throws IOException, InterruptedException {
-		throw new UnsupportedOperationException("Not Implemented");
+		HttpRequest httpRequest = HttpRequest.newBuilder()
+				.uri(requestUrl)
+				.GET()
+				.build();
+
+		return httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofInputStream());
 	}
 
 	@Override
 	public <T> T parseJsonResponse(InputStream responseBody, Type targetType) {
-		throw new UnsupportedOperationException("Not Implemented");
+		try (InputStreamReader inputStreamReader = new InputStreamReader(responseBody)) {
+			Gson gson = new Gson();
+			return gson.fromJson(inputStreamReader, targetType);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
