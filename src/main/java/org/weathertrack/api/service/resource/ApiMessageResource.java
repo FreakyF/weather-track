b71b1551cfc4d4c013471867a308e8da50e1c70e
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-03 18:47:19 +0200

    #6: Create ApiMessageResource

---
+++ src/main/java/org/weathertrack/api/service/resource/ApiMessageResource.java
@@ -0,0 +1,9 @@
+package org.weathertrack.api.service.resource;
+
+public class ApiMessageResource {
+	public static final String NO_CITIES_FOUND
+			= "No cities found!";
+
+	private ApiMessageResource() {
+	}
+}
