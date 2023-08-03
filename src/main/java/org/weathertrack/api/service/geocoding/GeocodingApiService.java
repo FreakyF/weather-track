74d4fd9f2ed94629927064bb0690530d670739fc
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-03 21:16:09 +0200

    #6: Add description to fetchCitiesForCityName.

--- src/main/java/org/weathertrack/api/service/geocoding/GeocodingApiService.java
+++ src/main/java/org/weathertrack/api/service/geocoding/GeocodingApiService.java
@@ -7,7 +7,12 @@ import org.weathertrack.model.ResponseData;
 import java.util.List;
 
 public interface GeocodingApiService {
-	ResponseData<List<CityDataDTO>> fetchCitiesForCityName(String cityName);
+	/**
+	 * @param cityName name of the city to get detailed data for.
+	 * @return List of detailed information for cities with given name.
+	 * @throws IllegalArgumentException Throwed when cityName is null or is blank.
+	 */
+	ResponseData<List<CityDataDTO>> fetchCitiesForCityName(String cityName) throws IllegalArgumentException;
 
 	ResponseData<CityDataDTO> fetchGeocodingDataForCity(GetCityDataRequest request);
 }
