f0f37c26793d2d03e89d58779631da37c491ffd1
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-07 17:07:39 +0200

    #6: Remove blank line.

--- src/main/java/org/weathertrack/forecast/ForecastModule.java
+++ src/main/java/org/weathertrack/forecast/ForecastModule.java
@@ -4,9 +4,8 @@ import com.google.inject.AbstractModule;
 import org.weathertrack.forecast.api.ApiForecastProvider;
 
 public class ForecastModule extends AbstractModule {
-    @Override
-    protected void configure() {
-
-        bind(ForecastProvider.class).to(ApiForecastProvider.class);
-    }
+	@Override
+	protected void configure() {
+		bind(ForecastProvider.class).to(ApiForecastProvider.class);
+	}
 }
