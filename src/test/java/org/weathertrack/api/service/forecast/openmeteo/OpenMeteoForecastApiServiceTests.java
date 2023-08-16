2470de24a86584f3a5cee44edac400d5de7f3f36
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-16 21:34:00 +0200

    #37: Rename methods.

--- src/test/java/org/weathertrack/api/service/forecast/openmeteo/OpenMeteoForecastApiServiceTests.java
+++ src/test/java/org/weathertrack/api/service/forecast/openmeteo/OpenMeteoForecastApiServiceTests.java
@@ -4,11 +4,18 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.weathertrack.TestData;
 import org.weathertrack.api.service.forecast.openmeteo.model.WeatherReport;
+import org.weathertrack.api.service.geocoding.model.GeocodingCityData;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
 
 @ExtendWith(MockitoExtension.class)
 class OpenMeteoForecastApiServiceTests {
@@ -27,11 +34,22 @@ class OpenMeteoForecastApiServiceTests {
 	@AfterEach
 	void afterEach() throws Exception {
 		closeable.close();
-
 	}
 
 	@Test
-	void fetchForecastForCoordinates_WhenGeocodingCityDataIsNull_ShouldThrowException_WithAppropriateMessage() {
+	void fetchForecastForCoordinates_WhenGeocodingCityDataIsNull_ShouldThrowNullPointerException() {
+		// When
+		var mockGeocodingCityData = new GeocodingCityData("Kielce", "Świętokrzyskie", "Poland", 21, 37);
+
+		// Given
+		var result = sut.fetchForecastForCoordinates(mockGeocodingCityData);
+
+		// Then
+	}
+
+	// TODO: Add what exception should be thrown.
+	@Test
+	void fetchForecastForCoordinates_WhenUriSyntaxIsInvalid_ShouldThrowException() {
 		// When
 
 		// Given
@@ -40,7 +58,7 @@ class OpenMeteoForecastApiServiceTests {
 	}
 
 	@Test
-	void fetchForecastForCoordinates_WhenUriSyntaxIsInvalid_ShouldThrowException_WithAppropriateMessage() {
+	void fetchForecastForCoordinates_WhenGeocodingCityDataAndUriIsValid_ShouldReturnSuccessfulResponseData() {
 		// When
 
 		// Given
@@ -49,7 +67,7 @@ class OpenMeteoForecastApiServiceTests {
 	}
 
 	@Test
-	void fetchForecastForCoordinates_WhenGeocodingCityDataAndUriIsValid_ShouldReturnResponseData() {
+	void fetchForecastForCoordinates_WhenWeatherReportIsNull_ShouldThrowNullPointerException() {
 		// When
 
 		// Given
@@ -58,7 +76,7 @@ class OpenMeteoForecastApiServiceTests {
 	}
 
 	@Test
-	void fetchForecastForCoordinates_WhenWeatherReportIsNull_ShouldThrowException_WithAppropriateMessage() {
+	void fetchForecastForCoordinates_WhenWeatherReportIsEmpty_ShouldThrowException() {
 		// When
 
 		// Given
@@ -66,26 +84,34 @@ class OpenMeteoForecastApiServiceTests {
 		// Then
 	}
 
-	@Test
-	void fetchForecastForCoordinates_WhenWeatherReportIsEmpty_ShouldThrowException_WithAppropriateMessage() {
+	private static Stream<Arguments> fetchForecastForCoordinates_WhenStatusCodeIsReceived_ShouldReturnResponseData() {
+		return Stream.of(
+				Arguments.of()
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource
+	void fetchForecastForCoordinates_WhenStatusCodeIsReceived_ShouldReturnResponseData(
+			int statusCodeValue, boolean success, String responseMessage, ArrayList<GeocodingCityData> expectedCityDataResponse) {
 		// When
 
 		// Given
 
 		// Then
 	}
+	// TODO: Add what exception should be thrown.
 
-	@Test
-	void fetchForecastForCoordinates_WhenStatusCodeIsReceived_ShouldReturnResponseData_WithAppropriateMessage() {
-		// When
-
-		// Given
-
-		// Then
+	private static Stream<Arguments> fetchForecastForCoordinates_WhenStatusCodeIsReceived_ShouldThrowException() {
+		return Stream.of(
+				Arguments.of()
+		);
 	}
 
-	@Test
-	void fetchForecastForCoordinates_WhenStatusCodeIsReceived_ShouldThrowException_WithAppropriateMessage() {
+	@ParameterizedTest
+	@MethodSource
+	void fetchForecastForCoordinates_WhenStatusCodeIsReceived_ShouldThrowException(
+			int statusCodeValue, boolean success, String responseMessage, ArrayList<GeocodingCityData> expectedCityDataResponse) {
 		// When
 
 		// Given
