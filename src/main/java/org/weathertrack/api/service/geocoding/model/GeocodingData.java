d6b1971efa045f1cf22587bf8e2409987ef80ff8
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-16 15:36:15 +0200

    #36: Convert class to a record.

--- src/main/java/org/weathertrack/api/service/geocoding/model/GeocodingData.java
+++ src/main/java/org/weathertrack/api/service/geocoding/model/GeocodingData.java
@@ -1,19 +1,5 @@
 package org.weathertrack.api.service.geocoding.model;
 
-public class GeocodingData {
-	private final double longitude;
-	private final double latitude;
+public record GeocodingData(double longitude, double latitude) {
 
-	public GeocodingData(double longitude, double latitude) {
-		this.longitude = longitude;
-		this.latitude = latitude;
-	}
-
-	public double getLongitude() {
-		return longitude;
-	}
-
-	public double getLatitude() {
-		return latitude;
-	}
 }
