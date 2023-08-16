23a042aded5c9db516a9900407ea26622e0db3c4
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-16 15:36:20 +0200

    #36: Convert class to a record.

--- src/main/java/org/weathertrack/api/service/geocoding/openmeteo/model/GetCityDataRequest.java
+++ src/main/java/org/weathertrack/api/service/geocoding/openmeteo/model/GetCityDataRequest.java
@@ -1,19 +1,5 @@
 package org.weathertrack.api.service.geocoding.openmeteo.model;
 
-public class GetCityDataRequest {
-	private final double longitude;
-	private final double latitude;
+public record GetCityDataRequest(double longitude, double latitude) {
 
-	public GetCityDataRequest(double longitude, double latitude) {
-		this.longitude = longitude;
-		this.latitude = latitude;
-	}
-
-	public double getLongitude() {
-		return longitude;
-	}
-
-	public double getLatitude() {
-		return latitude;
-	}
 }
