92fcbaa4f7494d6924160f6f69aa471758fa6fc2
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-16 20:22:59 +0200

    #37: Prototype fetchForecastForCoordinates unit tests.

--- src/test/java/org/weathertrack/api/service/forecast/openmeteo/OpenMeteoForecastApiServiceTests.java
+++ src/test/java/org/weathertrack/api/service/forecast/openmeteo/OpenMeteoForecastApiServiceTests.java
@@ -2,13 +2,14 @@ package org.weathertrack.api.service.forecast.openmeteo;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class OpenMeteoForecastApiServiceTests {
+class OpenMeteoForecastApiServiceTests {
 	private AutoCloseable closeable;
 	@InjectMocks
 	private OpenMeteoForecastApiService sut;
@@ -23,5 +24,68 @@ public class OpenMeteoForecastApiServiceTests {
 	void afterEach() throws Exception {
 		closeable.close();
 
+	}
+
+	@Test
+	void fetchForecastForCoordinates_WhenGeocodingCityDataIsNull_ShouldThrowException_WithAppropriateMessage() {
+		// When
+
+		// Given
+
+		// Then
+	}
+
+	@Test
+	void fetchForecastForCoordinates_WhenUriSyntaxIsInvalid_ShouldThrowException_WithAppropriateMessage() {
+		// When
+
+		// Given
+
+		// Then
+	}
+
+	@Test
+	void fetchForecastForCoordinates_WhenGeocodingCityDataAndUriIsValid_ShouldReturnResponseData() {
+		// When
+
+		// Given
+
+		// Then
+	}
+
+	@Test
+	void fetchForecastForCoordinates_WhenWeatherReportIsNull_ShouldThrowException_WithAppropriateMessage() {
+		// When
+
+		// Given
+
+		// Then
+	}
+
+	@Test
+	void fetchForecastForCoordinates_WhenWeatherReportIsEmpty_ShouldThrowException_WithAppropriateMessage() {
+		// When
+
+		// Given
+
+		// Then
+	}
+
+	@Test
+	void fetchForecastForCoordinates_WhenStatusCodeIsReceived_ShouldReturnResponseData_WithAppropriateMessage() {
+		// When
+
+		// Given
+
+		// Then
+	}
+
+	@Test
+	void fetchForecastForCoordinates_WhenStatusCodeIsReceived_ShouldThrowException_WithAppropriateMessage() {
+		// When
+
+		// Given
+
+		// Then
 	}
 }
