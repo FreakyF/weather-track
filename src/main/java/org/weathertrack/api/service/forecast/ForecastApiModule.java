d56f92653d7f288f9a010a5d0c48a1e312e3c633
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-17 18:42:50 +0200

    #37: fIX ForecastApiModule by changin names of fields.

--- src/main/java/org/weathertrack/api/service/forecast/ForecastApiModule.java
+++ src/main/java/org/weathertrack/api/service/forecast/ForecastApiModule.java
@@ -5,17 +5,17 @@ import com.google.inject.name.Names;
 import org.apache.hc.core5.net.URIBuilder;
 
 public class ForecastApiModule extends AbstractModule {
-	public static final String ANNOTATION_FORECAST_API = "ANNOTATION_GEOCODING_API";
-	private static final String GEOCODING_API_SCHEME = "https";
-	private static final String GEOCODING_API_HOST = "https://api.open-meteo.com";
-	private static final String GEOCODING_API_PATH = "/v1/forecast";
+	public static final String ANNOTATION_FORECAST_API = "ANNOTATION_FORECAST_API";
+	private static final String FORECAST_API_SCHEME = "https";
+	private static final String FORECAST_API_HOST = "api.open-meteo.com";
+	private static final String FORECAST_API_PATH = "/v1/forecast";
 
 	@Override
 	protected void configure() {
-		var geocodingApiServiceUriBuilder = new URIBuilder()
-				.setScheme(GEOCODING_API_SCHEME)
-				.setHost(GEOCODING_API_HOST)
-				.setPath(GEOCODING_API_PATH);
-		bind(URIBuilder.class).annotatedWith(Names.named(ANNOTATION_FORECAST_API)).toInstance(geocodingApiServiceUriBuilder);
+		var forecastApiServiceUriBuilder = new URIBuilder()
+				.setScheme(FORECAST_API_SCHEME)
+				.setHost(FORECAST_API_HOST)
+				.setPath(FORECAST_API_PATH);
+		bind(URIBuilder.class).annotatedWith(Names.named(ANNOTATION_FORECAST_API)).toInstance(forecastApiServiceUriBuilder);
 	}
 }
