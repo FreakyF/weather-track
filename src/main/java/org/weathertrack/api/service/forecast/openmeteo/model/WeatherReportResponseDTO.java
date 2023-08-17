e62b6ff04aba6dc04fac1833d18c043f5d5177a5
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-17 17:07:14 +0200

    #37: Remove field type from List<WeatherReport> to WeatherReport.

--- src/main/java/org/weathertrack/api/service/forecast/openmeteo/model/WeatherReportResponseDTO.java
+++ src/main/java/org/weathertrack/api/service/forecast/openmeteo/model/WeatherReportResponseDTO.java
@@ -1,11 +1,9 @@
 package org.weathertrack.api.service.forecast.openmeteo.model;
 
-import java.util.List;
-
 public class WeatherReportResponseDTO {
-	private List<WeatherReport> results;
+	private WeatherReport results;
 
-	public List<WeatherReport> getResults() {
+	public WeatherReport getResults() {
 		return results;
 	}
 }
