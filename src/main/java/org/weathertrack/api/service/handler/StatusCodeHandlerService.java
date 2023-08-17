6a62523a01c7e0a5fa02e752d7ca7f911fa21f33
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-17 17:52:32 +0200

    #37: Remove StatusCodeHandlerService interface.

--- src/main/java/org/weathertrack/api/service/handler/StatusCodeHandlerService.java
+++
@@ -1,9 +0,0 @@
-package org.weathertrack.api.service.handler;
-
-import org.weathertrack.api.service.exception.BadRequestException;
-import org.weathertrack.api.service.exception.NotFoundException;
-import org.weathertrack.model.ResponseData;
-
-public interface StatusCodeHandlerService<V> {
-	ResponseData<V> handleStatusCode(int statusCode) throws BadRequestException, NotFoundException;
-}
