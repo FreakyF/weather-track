8ea122db38fd08a0f4338b46038570c015e9f89d
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-17 16:01:54 +0200

    #37: Implement dependency injection for OpenMeteoForecastApiService.

--- src/main/java/org/weathertrack/api/service/forecast/openmeteo/OpenMeteoForecastApiService.java
+++ src/main/java/org/weathertrack/api/service/forecast/openmeteo/OpenMeteoForecastApiService.java
@@ -1,11 +1,27 @@
 package org.weathertrack.api.service.forecast.openmeteo;
 
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import org.apache.hc.core5.net.URIBuilder;
+import org.weathertrack.api.service.forecast.ForecastApiModule;
 import org.weathertrack.api.service.forecast.ForecastApiService;
 import org.weathertrack.api.service.forecast.model.WeatherData;
 import org.weathertrack.api.service.geocoding.model.GeocodingCityData;
+import org.weathertrack.api.service.http.HttpService;
 import org.weathertrack.model.ResponseData;
 
 public class OpenMeteoForecastApiService implements ForecastApiService {
+	private final URIBuilder uriBuilder;
+	private final HttpService httpService;
+
+	@Inject
+	public OpenMeteoForecastApiService(
+			@Named(ForecastApiModule.ANNOTATION_FORECAST_API) URIBuilder uriBuilder,
+			HttpService httpService) {
+		this.uriBuilder = uriBuilder;
+		this.httpService = httpService;
+	}
+
 	@Override
 	public ResponseData<WeatherData> fetchForecastForCoordinates(GeocodingCityData geocodingCityData) {
 		throw new UnsupportedOperationException("Not implemented");
