c2cb1eb4b0330706694801b56d0e0c4423628568
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-17 18:38:23 +0200

    #37: Add ForecastApiModule.

--- src/main/java/org/weathertrack/Main.java
+++ src/main/java/org/weathertrack/Main.java
@@ -3,6 +3,7 @@ package org.weathertrack;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.weathertrack.api.service.ApiServiceModule;
+import org.weathertrack.api.service.forecast.ForecastApiModule;
 import org.weathertrack.api.service.geocoding.GeocodingApiModule;
 import org.weathertrack.input.service.userio.UserIOModule;
 import org.weathertrack.logging.LoggingModule;
@@ -12,7 +13,8 @@ public class Main {
 			new LoggingModule(),
 			new UserIOModule(),
 			new ApiServiceModule(),
-			new GeocodingApiModule()
+			new GeocodingApiModule(),
+			new ForecastApiModule()
 	);
 
 	public static void main(String[] args) {
