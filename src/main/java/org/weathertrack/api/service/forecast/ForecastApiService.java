47238f71144b12307f421295b34dcc7bbcc62517
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-17 17:07:53 +0200

    #37: Change method type from ResponseData<WeatherData> to WeatherReport.

--- src/main/java/org/weathertrack/api/service/forecast/ForecastApiService.java
+++ src/main/java/org/weathertrack/api/service/forecast/ForecastApiService.java
@@ -1,9 +1,13 @@
 package org.weathertrack.api.service.forecast;
 
-import org.weathertrack.api.service.forecast.model.WeatherData;
+import org.weathertrack.api.service.exception.BadRequestException;
+import org.weathertrack.api.service.exception.NotFoundException;
+import org.weathertrack.api.service.forecast.openmeteo.model.WeatherReport;
 import org.weathertrack.api.service.geocoding.model.GeocodingCityData;
 import org.weathertrack.model.ResponseData;
 
+import java.io.IOException;
+
 public interface ForecastApiService {
-	ResponseData<WeatherData> fetchForecastForCoordinates(GeocodingCityData geocodingCityData);
+	ResponseData<WeatherReport> fetchForecastForCoordinates(GeocodingCityData geocodingCityData) throws BadRequestException, NotFoundException, IOException, InterruptedException;
 }
