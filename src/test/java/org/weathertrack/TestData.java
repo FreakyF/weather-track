2c3194c80e6a1a056a98f9b5eb4ced40b53acbc1
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-19 14:34:00 +0200

    #37: Implement createForecastReport TestData.

--- src/test/java/org/weathertrack/TestData.java
+++ src/test/java/org/weathertrack/TestData.java
@@ -3,12 +3,19 @@ package org.weathertrack;
 import org.weathertrack.api.service.forecast.model.ForecastData;
 import org.weathertrack.api.service.forecast.model.Unit;
 import org.weathertrack.api.service.forecast.model.WeatherRecord;
+import org.weathertrack.api.service.forecast.openmeteo.model.Daily;
+import org.weathertrack.api.service.forecast.openmeteo.model.ForecastReport;
+import org.weathertrack.api.service.forecast.openmeteo.model.Hourly;
 import org.weathertrack.api.service.geocoding.model.GeocodingCityData;
 import org.weathertrack.api.service.geocoding.openmeteo.model.CityDataDTO;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TestData {
 	public static class Provider {
@@ -66,9 +73,102 @@ public class TestData {
 
 			builder.setUtcOffsetSeconds(3600);
 
-			builder.setTimeZone(ZoneId.of("UTC"));
+			builder.setTimeZone(ZoneId.of("Europe/Warsaw"));
 
 			return builder.build();
+		}
+
+		public static ForecastReport createForecastReport() {
+			Map<String, String> hourlyUnits = new HashMap<>();
+			hourlyUnits.put("temperature_2m", "°C");
+			hourlyUnits.put("relativehumidity_2m", "%");
+
+			Hourly hourly = createHourly();
+
+			Map<String, String> dailyUnits = new HashMap<>();
+			dailyUnits.put("temperature_2m_max", "°C");
+			dailyUnits.put("precipitation_probability_max", "%");
+
+			Daily daily = createDaily();
+
+			return new ForecastReport(21,
+					37,
+					3600,
+					3600,
+					"Europe/Warsaw",
+					"UTC",
+					50,
+					hourlyUnits,
+					hourly,
+					dailyUnits,
+					daily
+			);
+		}
+
+		private static Hourly createHourly() {
+			List<String> time = new ArrayList<>();
+			time.add("2023-08-19T12:00:00");
+			time.add("2023-08-19T13:00:00");
+
+			List<Double> temperature_2m = new ArrayList<>();
+			temperature_2m.add(25.0);
+			temperature_2m.add(26.5);
+
+			List<Integer> relativehumidity_2m = new ArrayList<>();
+			relativehumidity_2m.add(60);
+			relativehumidity_2m.add(55);
+
+			List<Double> precipitation = new ArrayList<>();
+			precipitation.add(0.0);
+			precipitation.add(0.1);
+
+			List<Integer> weathercode = new ArrayList<>();
+			weathercode.add(0);
+			weathercode.add(1);
+
+			List<Double> surface_pressure = new ArrayList<>();
+			surface_pressure.add(1013.25);
+			surface_pressure.add(1012.75);
+
+			List<Double> windspeed_10m = new ArrayList<>();
+			windspeed_10m.add(5.0);
+			windspeed_10m.add(6.0);
+
+			return new Hourly(time,
+					temperature_2m,
+					relativehumidity_2m,
+					precipitation,
+					weathercode,
+					surface_pressure,
+					windspeed_10m);
+		}
+
+		private static Daily createDaily() {
+			List<String> time = new ArrayList<>();
+			time.add("2023-08-19");
+			time.add("2023-08-20");
+
+			List<Integer> weathercode = new ArrayList<>();
+			weathercode.add(0);
+			weathercode.add(1);
+
+			List<Double> temperature_2m_max = new ArrayList<>();
+			temperature_2m_max.add(28.0);
+			temperature_2m_max.add(29.5);
+
+			List<Integer> precipitation_probability_max = new ArrayList<>();
+			precipitation_probability_max.add(20);
+			precipitation_probability_max.add(30);
+
+			List<Double> windspeed_10m_max = new ArrayList<>();
+			windspeed_10m_max.add(8.0);
+			windspeed_10m_max.add(9.0);
+
+			return new Daily(time,
+					weathercode,
+					temperature_2m_max,
+					precipitation_probability_max,
+					windspeed_10m_max);
 		}
 	}
 
