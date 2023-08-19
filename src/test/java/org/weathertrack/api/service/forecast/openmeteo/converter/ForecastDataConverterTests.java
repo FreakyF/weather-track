fa5e01f10f025074166070c75febfbac648bab79
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-19 17:29:40 +0200

    #37: Implement unit tests for ForecastDataConverterTests.

--- src/test/java/org/weathertrack/api/service/forecast/openmeteo/converter/ForecastDataConverterTests.java
+++ src/test/java/org/weathertrack/api/service/forecast/openmeteo/converter/ForecastDataConverterTests.java
@@ -1,4 +1,62 @@
 package org.weathertrack.api.service.forecast.openmeteo.converter;
 
-public class ForecastDataConverterTests {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.weathertrack.TestData;
+import org.weathertrack.api.service.exception.ApiServiceExceptionMessage;
+import org.weathertrack.api.service.forecast.model.ForecastData;
+import org.weathertrack.api.service.forecast.openmeteo.model.ForecastReport;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+class ForecastDataConverterTests {
+	private ForecastReport MOCKED_FORECAST_REPORT;
+	private static ForecastData MOCKED_FORECAST_DATA;
+	private AutoCloseable closeable;
+
+	@BeforeEach
+	void beforeEach() {
+		closeable = MockitoAnnotations.openMocks(this);
+		MOCKED_FORECAST_REPORT = TestData.Provider.createForecastReport();
+		MOCKED_FORECAST_DATA = TestData.Provider.createForecastData();
+	}
+
+	@AfterEach
+	void afterEach() throws Exception {
+		closeable.close();
+	}
+
+	@Test
+	void forecastReportToForecastData_WhenForecastReportIsNull_ShouldThrowNullPointerException() {
+		// Given
+		var thrown = assertThrows(
+				Exception.class,
+				() -> ForecastDataConverter.forecastReportToForecastData(null),
+				"Expected forecastReportToForecastData to throw Exception, but it didn't"
+		);
+
+		// Then
+		assertTrue(thrown instanceof RuntimeException, "Expected NullPointerException");
+		assertEquals(NullPointerException.class, thrown.getClass());
+		assertEquals(ApiServiceExceptionMessage.FORECAST_REPORT_DATA_IS_NULL, thrown.getMessage());
+	}
+
+	@Test
+	void forecastReportToForecastData_WhenForecastReportIsValid_ShouldReturnForecastData() {
+		// When
+		var expectedResult = MOCKED_FORECAST_DATA;
+
+		// Given
+		var result = ForecastDataConverter.forecastReportToForecastData(MOCKED_FORECAST_REPORT);
+
+		// Then
+		assertEquals(expectedResult, result);
+	}
 }
