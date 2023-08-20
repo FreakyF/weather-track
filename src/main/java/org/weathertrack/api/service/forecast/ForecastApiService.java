6af8966aaaf8af81d87baeee515ec7302cd9ddbf
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-20 16:30:00 +0200

    #37: Document fetchForecastForCoordinates.

--- src/main/java/org/weathertrack/api/service/forecast/ForecastApiService.java
+++ src/main/java/org/weathertrack/api/service/forecast/ForecastApiService.java
@@ -10,5 +10,14 @@ import java.io.IOException;
 
 public interface ForecastApiService {
 	// TODO: Document this method.
+
+	/**
+	 * @param geocodingCityData contains the latitude and longitude of the location for which the forecast is requested.
+	 * @return A {@link ResponseData} object containing the forecast data for the given coordinates.
+	 * @throws BadRequestException  when statusCode is 400.
+	 * @throws NotFoundException    when statusCode is 404.
+	 * @throws IOException          when sendHttpGetRequest returns null.
+	 * @throws InterruptedException when sendHttpGetRequest is interrupted.
+	 */
 	ResponseData<ForecastData> fetchForecastForCoordinates(GeocodingCityData geocodingCityData) throws BadRequestException, NotFoundException, IOException, InterruptedException;
 }
