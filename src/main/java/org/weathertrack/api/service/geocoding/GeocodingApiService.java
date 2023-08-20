764264cba11d63fb356647749869bbad8ac33e47
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-20 15:36:27 +0200

    #37: Fix logic error in docs.

--- src/main/java/org/weathertrack/api/service/geocoding/GeocodingApiService.java
+++ src/main/java/org/weathertrack/api/service/geocoding/GeocodingApiService.java
@@ -9,13 +9,11 @@ import java.io.IOException;
 import java.util.List;
 
 public interface GeocodingApiService {
-	// TODO: Improve documentation. Logic is not correct. sendHttpGetRequest CANNOT BE null.
-
 	/**
 	 * @param cityName name of the city to get detailed data for.
 	 * @return List of detailed information for cities with given name.
 	 * @throws IllegalArgumentException when cityName is null or is blank.
-	 * @throws IOException              when sendHttpGetRequest is null.
+	 * @throws IOException              when sendHttpGetRequest returns null.
 	 * @throws InterruptedException     when sendHttpGetRequest is interrupted.
 	 */
 	ResponseData<List<GeocodingCityData>> fetchCitiesForCityName(String cityName) throws IllegalArgumentException, IOException, InterruptedException, BadRequestException, NotFoundException;
