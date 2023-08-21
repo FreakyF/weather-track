96362541ce59520a6c23dc4ba414d584fd452239
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-21 21:08:58 +0200

    #37: Use OpenMeteoApiParameterBuilder for building the ForecastApiUri.

--- src/main/java/org/weathertrack/api/service/forecast/openmeteo/OpenMeteoForecastApiService.java
+++ src/main/java/org/weathertrack/api/service/forecast/openmeteo/OpenMeteoForecastApiService.java
@@ -12,16 +12,15 @@ import org.weathertrack.api.service.forecast.ForecastApiService;
 import org.weathertrack.api.service.forecast.model.ForecastData;
 import org.weathertrack.api.service.forecast.openmeteo.converter.ForecastDataConverter;
 import org.weathertrack.api.service.forecast.openmeteo.model.ForecastReport;
-import org.weathertrack.api.service.forecast.openmeteo.resource.ApiParametersResource;
 import org.weathertrack.api.service.geocoding.model.GeocodingCityData;
 import org.weathertrack.api.service.http.HttpService;
 import org.weathertrack.api.service.http.exception.ParseJsonException;
+import org.weathertrack.api.service.uri.builder.openmeteo.OpenMeteoApiParameterBuilder;
 import org.weathertrack.model.Response;
 import org.weathertrack.model.ResponseData;
 
 import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 
 import static org.weathertrack.api.service.HttpStatusCodeHandler.handleStatusCode;
 
@@ -78,17 +77,21 @@ public class OpenMeteoForecastApiService implements ForecastApiService {
 	private URI buildForecastApiUri(GeocodingCityData geocodingCityData) {
 		var latitude = geocodingCityData.latitude();
 		var longitude = geocodingCityData.longitude();
-		try {
-			return uriBuilder
-					.setParameter(ApiParametersResource.LATITUDE, String.valueOf(latitude))
-					.setParameter(ApiParametersResource.LONGITUDE, String.valueOf(longitude))
-					.setParameter(ApiParametersResource.HOURLY, "temperature_2m,relativehumidity_2m,precipitation,weathercode,surface_pressure,windspeed_10m")
-					.setParameter(ApiParametersResource.DAILY, "weathercode,temperature_2m_max,precipitation_probability_max,windspeed_10m_max")
-					.setParameter(ApiParametersResource.TIMEZONE, "auto")
-					.build();
-			// TODO: Create a OpenMeteoApiParameterBuilder. Apply builder pattern. You should choose what you want to include in the request, for eg. if you want to include temperature_2m you should have method AddTemperature() etc.
-		} catch (URISyntaxException e) {
-			throw new IllegalArgumentException(ApiServiceExceptionMessage.URI_SYNTAX_IS_INVALID);
-		}
+
+		return new OpenMeteoApiParameterBuilder.Builder()
+				.setLatitude(latitude)
+				.setLongitude(longitude)
+				.setHourlyTemperature()
+				.setHourlyRelativeHumidity()
+				.setHourlyPrecipitation()
+				.setHourlyWeatherCode()
+				.setHourlySurfacePressure()
+				.setHourlyWindSpeed()
+				.setDailyMaxTemperature()
+				.setDailyMaxPrecipitation()
+				.setDailyWeatherCode()
+				.setDailyMaxWindSpeed()
+				.setAutoTimeZone()
+				.buildURI(uriBuilder);
 	}
 }
