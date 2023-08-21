34f0567fc95aa67d8eda8613827826bf497e07ab
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-21 22:07:55 +0200

    #37: Rename file to ApiParameters.

--- src/main/java/org/weathertrack/api/service/forecast/openmeteo/builder/ApiParameters.java
+++ src/main/java/org/weathertrack/api/service/forecast/openmeteo/builder/ApiParameters.java
@@ -1,12 +1,12 @@
 package org.weathertrack.api.service.forecast.openmeteo.builder;
 
-public class ApiParametersResource {
+public class ApiParameters {
 	public static final String LATITUDE = "latitude";
 	public static final String LONGITUDE = "longitude";
 	public static final String HOURLY = "hourly";
 	public static final String DAILY = "daily";
 	public static final String TIMEZONE = "timezone";
 
-	private ApiParametersResource() {
+	private ApiParameters() {
 	}
 }
