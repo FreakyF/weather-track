ae90fefabe181c543b3782639bc8a0adef957022
Kamil Fudala <kamil.fudala44@gmail.com>
2023-08-21 17:23:31 +0200

    #37: Create ApiParametersResource.

---
+++ src/main/java/org/weathertrack/api/service/forecast/openmeteo/resource/ApiParametersResource.java
@@ -0,0 +1,12 @@
+package org.weathertrack.api.service.forecast.openmeteo.resource;
+
+public class ApiParametersResource {
+	public static final String LATITUDE = "latitude";
+	public static final String LONGITUDE = "longitude";
+	public static final String HOURLY = "hourly";
+	public static final String DAILY = "daily";
+	public static final String TIMEZONE = "timezone";
+
+	private ApiParametersResource() {
+	}
+}
