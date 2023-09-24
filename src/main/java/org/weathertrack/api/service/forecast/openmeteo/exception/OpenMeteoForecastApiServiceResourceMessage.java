174fa8a7e8b13f95f34b5a3f2945160b5bf08067
Kamil Fudala <kamil.fudala44@gmail.com>
2023-09-24 15:22:29 +0200

    #37: Create OpenMeteoForecastApiServiceResourceMessage static class that holds localised messages for OpenMeteoForecastApiService class.

---
+++ src/main/java/org/weathertrack/api/service/forecast/openmeteo/exception/OpenMeteoForecastApiServiceResourceMessage.java
@@ -0,0 +1,9 @@
+package org.weathertrack.api.service.forecast.openmeteo.exception;
+
+public class OpenMeteoForecastApiServiceResourceMessage {
+	public static final String FORECAST_API_SERVICE_LOCALIZATION_NOT_INITIALIZED
+			= "Error while trying to initialize Open Meteo forecast api service localization";
+
+	private OpenMeteoForecastApiServiceResourceMessage() {
+	}
+}
