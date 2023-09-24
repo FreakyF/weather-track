727d18fe84297352f927b4e0db44c141b97d0578
Kamil Fudala <kamil.fudala44@gmail.com>
2023-09-24 11:46:33 +0200

    #37: Replace error message String with a static String from ForecastInterpreterMessage class.

--- src/main/java/org/weathertrack/api/service/forecast/openmeteo/resource/ForecastInterpreter.java
+++ src/main/java/org/weathertrack/api/service/forecast/openmeteo/resource/ForecastInterpreter.java
@@ -1,5 +1,6 @@
 package org.weathertrack.api.service.forecast.openmeteo.resource;
 
+import org.weathertrack.api.service.forecast.openmeteo.exception.ForecastIntepreterMessage;
 import org.weathertrack.api.service.forecast.openmeteo.exception.NotInitializedException;
 import org.weathertrack.logging.Logger;
 import org.weathertrack.logging.factory.LoggerFactory;
@@ -36,7 +37,7 @@ public class ForecastInterpreter {
 
 	public static String interpretWeatherCode(int code) {
 		if (!initialized) {
-			throw new NotInitializedException("Error while trying to initialize weather codes descriptions");
+			throw new NotInitializedException(ForecastIntepreterMessage.FORECAST_DESCRIPTIONS_NOT_INITIALIZED);
 		}
 		return properties.getProperty(String.valueOf(code), defaultWeatherInterpretation);
 	}
