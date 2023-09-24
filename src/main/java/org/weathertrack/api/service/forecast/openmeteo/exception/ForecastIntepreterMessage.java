77b754efdb94e45e618760120138cadfed27ed8c
Kamil Fudala <kamil.fudala44@gmail.com>
2023-09-24 11:46:12 +0200

    #37: Create a static class that holds ForecastInterpreter error messages.

---
+++ src/main/java/org/weathertrack/api/service/forecast/openmeteo/exception/ForecastIntepreterMessage.java
@@ -0,0 +1,8 @@
+package org.weathertrack.api.service.forecast.openmeteo.exception;
+
+public class ForecastIntepreterMessage {
+	public static final String FORECAST_DESCRIPTIONS_NOT_INITIALIZED = "Error while trying to initialize weather codes descriptions";
+
+	private ForecastIntepreterMessage() {
+	}
+}
